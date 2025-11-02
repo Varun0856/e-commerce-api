d478f315d4bb565ed2622d11e5d5a241326d5b1d
Varun0856 <Varun0856@users.noreply.github.com>
2025-11-02 22:17:06 +0530

    feat: add auth middleware to verify access token

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import prisma from '../database/prisma.js'
+import { ApiError } from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/AsyncHandler.js"
+import { veirfyAccessToken, verifyAccessToken } from "../utils/tokenUtils.js"
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "");
+  if (!token) {
+    throw new ApiError(401, "No token found");
+  }
+  const decodedToken = verifyAccessToken(token);
+
+  if (!decodedToken?.userId) {
+    throw new ApiError(401, "Invalid or expired access token")
+  }
+
+  const user = prisma.user.findUnique({
+    where: { id: decodedToken.userId }
+  });
+
+  if (!user) {
+    throw new ApiError(401, "Unauthorized: Invalid access token");
+  };
+
+  req.user = user;
+  next();
+})
+
+
