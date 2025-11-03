2bc9a34b56c6a53cdb13c531cd516f7a90edcf9d
Varun0856 <Varun0856@users.noreply.github.com>
2025-11-03 22:57:42 +0530

    feat: add home endpoint with use of cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import express from 'express'
 import errorHandler from './middlewares/error.middleware.js'
+import cookieParser from 'cookie-parser'
+import authRouter from './routes/auth.route.js';
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandler);
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    baseURL: '/api/v1',
+    endpoints: {
+      register: 'POST api/v1/auth/register',
+      login: 'POST api/v1/auth/login',
+      logout: 'POST api/v1/auth/logout',
+      refreshAccessToken: 'POST api/v1/auth/token'
+    }
+  })
+})
+
+app.use('/api/v1/auth', authRouter);
 
 export default app;
