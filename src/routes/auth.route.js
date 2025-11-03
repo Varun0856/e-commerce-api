f1792ded8f0f98b66b0d377596cf3133f076b8b3
Varun0856 <Varun0856@users.noreply.github.com>
2025-11-03 23:04:43 +0530

    feat: routes for register, login, logout, refresh access token

---
+++ src/routes/auth.route.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/auth.controller.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
+const authRouter = Router();
+
+authRouter.post('/register', registerUser)
+authRouter.post('/login', loginUser)
+authRouter.post('/logout', verifyJWT, logoutUser)
+authRouter.post('/token', refreshAccessToken)
+
+export default authRouter;
