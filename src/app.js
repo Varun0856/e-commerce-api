516eb613bce0baa6c2053e6ace1298b9c7ab4132
Varun0856 <Varun0856@users.noreply.github.com>
2025-11-08 23:32:18 +0530

    feat: integrate productRouter with /api/v1/product endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import errorHandler from './middlewares/error.middleware.js'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.route.js';
+import productRouter from './routes/product.route.js';
 const app = express();
 
 
@@ -24,5 +25,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/product', productRouter);
 
 export default app;
