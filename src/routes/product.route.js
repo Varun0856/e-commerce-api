bef69233c79cc30413d846f1d9a3cd46118f36ac
Varun0856 <Varun0856@users.noreply.github.com>
2025-11-09 19:19:12 +0530

    feat: add get product by Id and update product route in the product.route.js

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { verifyJWT, checkRole } from "../middlewares/auth.middleware.js";
-import { getProducts, registerProduct } from "../controllers/product.controller.js";
+import { getProductById, getProducts, registerProduct, updateProduct } from "../controllers/product.controller.js";
 const productRouter = Router();
 
 productRouter.get('/list', getProducts);
+productRouter.get('/list/:id', getProductById);
 productRouter.post('/register', verifyJWT, checkRole('ADMIN'), registerProduct);
+productRouter.patch('/update/:id', verifyJWT, checkRole('ADMIN'), updateProduct);
 
 export default productRouter;
