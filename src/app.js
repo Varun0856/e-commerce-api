d1047d5832b745469543fadd41f08b3b2d999d52
Varun0856 <Varun0856@users.noreply.github.com>
2025-11-11 23:55:56 +0530

    update: use cart Router in the app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import errorHandler from './middlewares/error.middleware.js'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.route.js';
 import productRouter from './routes/product.route.js';
+import cartRouter from './routes/cart.route.js';
 const app = express();
 
 
@@ -26,5 +27,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/product', productRouter);
+app.use('/api/v1/cart', cartRouter);
 
 export default app;
